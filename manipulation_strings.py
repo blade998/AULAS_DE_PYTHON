d66aa06fca3ec17246dcdbb689e27aa54a0b9cec
Igor Santos <isantos@baymetrics.com.br>
2026-01-28 21:41:57 -0300

    novos scripst

---
+++ manipulation_strings.py
@@ -0,0 +1,10 @@
+name = input('Escreva seu nome ')
+
+print(f'o numero de caracteres name e :{len(name)}')
+print(f'Em maiusculo:{name.upper()}')
+print(f'Em minusculo:{name.lower()}')
+
+print(name[1:3])
+
+
+
